a4320ae9de556bde7020091a7797985e57bb8602
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-01-23 21:57:04 +0530

    updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/users",require("./routes/users"));
 app.use("/api/auth",require("./routes/auth"));
 app.use("/api/contacts",require("./routes/contacts"));
 
-if(process.env.NODE_ENV !== 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
     app.get("*", (req,res)=> res.sendFile(path.resolve(__dirname,"client","build", "index.html")));
 }
