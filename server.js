c7d60fcbce8a91e48ec61dc379fe39a0af95a72f
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-01-23 18:36:57 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app = express();
 const connectDB=require("./config/db");
 const cors=require("cors");
+const path=require("path");
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
@@ -16,15 +17,19 @@ connectDB();
 //body-parser
 app.use(express.json({extended:false}));
 
-app.get("/", function(req,res){
-    res.json({msg:"hello"});
-});
+// app.get("/", function(req,res){
+//     res.json({msg:"hello"});
+// });
 
 //Routes
 app.use("/api/users",require("./routes/users"));
 app.use("/api/auth",require("./routes/auth"));
 app.use("/api/contacts",require("./routes/contacts"));
 
+if(process.env.NODE_ENV !== 'production') {
+    app.use(express.static("client/build"));
+    app.get("*", (req,res)=> res.sendFile(path.resolve(__dirname,"client","build", "index.html")));
+}
 
 app.listen(PORT,function(){
     console.log("Server listening on port "+PORT);
